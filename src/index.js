fb0f9ec8f2b99477161d627b6d112d6e41d4c4d7
rehemasomo <rehemasomo30@gmail.com>
2024-03-31 22:04:50 +0300

    src-index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+    const form = document.getElementById("create-task-form");
+    const taskList = document.getElementById("tasks");
+  
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      const taskInput = document.getElementById("new-task-description");
+      const taskText = taskInput.value;
+      if (taskText !== "") {
+        renderTask(taskText);
+        taskInput.value = "";
+      }
+    });
+  
+    function renderTask(taskText) {
+      const taskItem = document.createElement("li");
+      taskItem.textContent = taskText;
+      taskList.appendChild(taskItem);
+    }
+  
 });
